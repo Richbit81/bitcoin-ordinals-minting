0df164da2fe8e0689846b203f634deb2937203bf
Thomas <thomasreichenbach@hotmail.com>
2026-08-02 01:16:45 +0200

    Fix twin-eligibility fetch under /freakheadzburn base path.
    Use Vite BASE_URL so JSON is not loaded from site root HTML.

---
+++ public/freakheadzburn/assets/cursor-DcbfQwRW.js
@@ -0,0 +1,3 @@
+import{w as o}from"./core-fjKqvQ38.js";import"./index-1zQafcmp.js";import"./index.es-BbGCWHqJ.js";import"./events-DQ172AOg.js";import"./index-nibyPLVP.js";const l=o` <svg fill="none" viewBox="0 0 13 4">
+  <path fill="currentColor" d="M.5 0h12L8.9 3.13a3.76 3.76 0 0 1-4.8 0L.5 0Z" />
+</svg>`;export{l as cursorSvg};
